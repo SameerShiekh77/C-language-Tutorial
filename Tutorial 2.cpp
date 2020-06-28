51eeecf4f8b93895cbe297a21f83baab3cce3cd9
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-06-28 15:24:14 +0500

    Here is the code of Tutorial 2 of C language

---
+++ Tutorial 2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	//printf is use for output purpose
+	
+//	printf("Hello World");
+
+//	int number = 7;
+//	printf("Here is my Lucky number: %d",number);
+
+	/*scanf is use for input purpose*/
+	
+//	int age;
+//	printf("Enter Your Age\n");
+//	scanf("%d",&age); //& is name is ampersand
+	//& take the address
+//	printf("Your age is: %d",age);
+
+	/*ESCAPE SEQUENCE CHARACTERS
+	1. \n = new line
+	2. \t = tab is equal to 8 spaces
+	3. \r = carriage return
+	4. \\ = backslash
+	5. \v = vertical tab
+	6. \b = backspace
+	*/
+	
+	//printf("Hello\nWorld");
+//	printf("\t\t\t\tHello World");
+//	printf("Hello\rWorld");
+//	printf("Hello\v ssdjflksdjf;lf asdklfjsad;lfWorld");
+	printf("Hello\bWorld");
+}
